08f2a118520a3cc2147f416d333e74496a6534b1
Fauzan <fauzankhan339@gmail.com>
2024-01-06 20:07:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+// const multer = require("multer");
+
+//database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database is connected successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//middlewares
+
+dotenv.config();
+app.use(express.json());
+connectDB();
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "https://blog-fauzan.vercel.app",methods:["POST","GET","PUT","DELETE"], credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+
+//image upload
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, fn) => {
+//     fn(null, "images");
+//   },
+//   filename: (req, file, fn) => {
+//     fn(null, req.body.img);
+//     // fn(null,"image.png")
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   res.status(200).json("Image has been uploaded successfully ");
+// });
+//
+port=process.env.PORT|| 5000
+app.listen(port, () => {
+  
+  console.log("app is running on port " + process.env.PORT);
+});
